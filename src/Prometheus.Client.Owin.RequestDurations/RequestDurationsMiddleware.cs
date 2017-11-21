a91c5c553d0f91cd19056ee8a1b9717d768a1c50
Sergey Kuznetsov <sergey.kuznetsov@hotmail.com>
2017-11-22 02:16:34 +0300

    create middleware prototype

---
+++ src/Prometheus.Client.Owin.RequestDurations/RequestDurationsMiddleware.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Prometheus.Client.Owin.RequestDurations
+{
+    public class RequestDurationsMiddleware
+    {
+        private static readonly string _metricHelpText = "duration histogram of http responses labeled with: ";
+
+        private readonly RequestDelegate _next;
+        private readonly RequestDurationsOptions _options;
+        private Histogram _histogram;
+
+        public RequestDurationsMiddleware(RequestDelegate next, RequestDurationsOptions options)
+        {
+            _next = next;
+            _options = options;
+
+            var labels = new List<string>();
+
+            if (_options.IncludeStatusCode)
+                labels.Add("status_code");
+
+            if (_options.IncludeMethod)
+                labels.Add("method");
+
+            if (_options.IncludePath)
+                labels.Add("path");
+
+            // create histogram with Registry            
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var route = context.Request.Path.ToString();
+            if (_options.ExcludeRoutes.Any(i => route.Contains(i)))
+            {
+                await _next.Invoke(context);
+                return;
+            }
+            var watch = Stopwatch.StartNew();
+
+            await _next.Invoke(context);
+
+            var method = context.Request.Method;
+            var statusCode = context.Response.StatusCode.ToString();
+
+            watch.Stop();
+            var seconds = watch.Elapsed.Seconds;
+            if (seconds > 0)
+            {
+                // write metric
+            }
+        }
+    }
+}
---
+++ src/Prometheus.Client.Owin.RequestDurations/RequestDurationsOptions.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+
+namespace Prometheus.Client.Owin.RequestDurations
+{
+    /// <summary>
+    ///     Options for RequestDurationsMiddleware
+    /// </summary>
+    public class RequestDurationsOptions
+    {
+        /// <summary>
+        ///      Metric name
+        /// </summary>
+        public string MetricName { get; set; }
+        
+        /// <summary>
+        ///     HTTP status code (200, 400, 404 etc.)
+        /// </summary>
+        public bool IncludeStatusCode { get; set; }
+        
+        /// <summary>
+        ///     HTTP method (GET, PUT, ...)
+        /// </summary>
+        public bool IncludeMethod { get; set; }
+        
+        /// <summary>
+        ///     URL path
+        /// </summary>
+        public bool IncludePath { get; set; }
+        
+        /// <summary>
+        ///     Ignore paths 
+        /// </summary>
+        public List<string> ExcludeRoutes { get; set; }
+        
+        /// <summary>
+        ///     Constructor
+        /// </summary>
+        public RequestDurationsOptions()
+        {
+            MetricName = "http_request_duration_seconds";
+            
+            IncludeStatusCode = true;
+            IncludeMethod = false;
+            IncludePath = false;
+            
+            ExcludeRoutes = new List<string>();
+        }
+    }
+}
