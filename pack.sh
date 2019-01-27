c50f4aa8d3c2fc4f46376679fd55805a1a160bdc
Sergey Kuznetsov <sergey.kuznetsov@hotmail.com>
2019-01-27 20:05:09 +0300

    Linux build

--- pack.sh
+++ pack.sh
@@ -1,6 +1,6 @@
 if [ ${APPVEYOR_REPO_TAG} == "true" ]
 then
-    dotnet pack ${APPVEYOR_BUILD_FOLDER}\src\Prometheus.Client.HttpRequestDurations -c Release --include-symbols --no-build -o artifacts\nuget
+    dotnet pack ${APPVEYOR_BUILD_FOLDER}/src/Prometheus.Client.HttpRequestDurations -c Release --include-symbols --no-build -o artifacts/nuget
 else
-   dotnet pack ${APPVEYOR_BUILD_FOLDER}\src\Prometheus.Client.HttpRequestDurations -c Release --include-symbols --no-build --version-suffix build${APPVEYOR_BUILD_NUMBER} -o artifacts\myget
+   dotnet pack ${APPVEYOR_BUILD_FOLDER}/src/Prometheus.Client.HttpRequestDurations -c Release --include-symbols --no-build --version-suffix build${APPVEYOR_BUILD_NUMBER} -o artifacts/myget
 fi
