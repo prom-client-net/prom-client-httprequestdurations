f311a6e2f2f18149c99e5012753c5c04c2ee8620
Sergey Kuznetsov <sergey.kuznetsov@hotmail.com>
2019-02-11 22:36:36 +0300

    Dependencies

--- src/Prometheus.Client.HttpRequestDurations/HttpRequestDurationsMiddleware.cs
+++ src/Prometheus.Client.HttpRequestDurations/HttpRequestDurationsMiddleware.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Prometheus.Client.HttpRequestDurations.Tools;
