c3bbfbc6161dfc287573ee605d2a221a7a2fbc3d
Serge K <cmt@phnx47.net>
2024-07-29 20:12:15 +0700

    test: label checks

--- tests/HttpRequestDurationsMiddlewareTests.cs
+++ tests/HttpRequestDurationsMiddlewareTests.cs
@@ -1,5 +1,6 @@
+using System;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Prometheus.Client.Collectors;
 using Xunit;
@@ -10,7 +11,6 @@ public class HttpRequestDurationsMiddlewareTests
 {
     private readonly ICollectorRegistry _registry;
     private readonly IApplicationBuilder _app;
-    private readonly HttpContext _ctx;
 
     public HttpRequestDurationsMiddlewareTests()
     {
@@ -19,44 +19,178 @@ public class HttpRequestDurationsMiddlewareTests
         var services = new ServiceCollection();
         services.AddSingleton(_registry);
         _app = new ApplicationBuilder(services.BuildServiceProvider());
-        _ctx = new DefaultHttpContext();
     }
 
     [Fact]
-    public void Default_Register_DefaultCollector()
+    public void Use_DefaultCollectorNotNull()
     {
-        _app.UsePrometheusRequestDurations();
-        _app.Build();
+        UseBuildApp();
 
-
-        _registry.TryGet(HttpRequestDurationsOptions.DefaultMetricName, out var collector);
+        _registry.TryGet(Defaults.MetricName, out var collector);
 
         Assert.NotNull(collector);
     }
-
-    [Fact]
-    public void CustomName_NotRegister_DefaultCollector()
-    {
-        _app.UsePrometheusRequestDurations(q => q.MetricName = "custom_name");
-        _app.Build();
-
-        _registry.TryGet(HttpRequestDurationsOptions.DefaultMetricName, out var collector);
-
-        Assert.Null(collector);
-    }
-
 
     [Theory]
     [InlineData("custom_name")]
     [InlineData("http_seconds")]
     [InlineData("myapp_http_request_duration_seconds")]
-    public void CustomName_Register_CustomCollector(string metricName)
+    public void Use_WithCustomMetricName_CustomCollectorNotNull(string metricName)
     {
-        _app.UsePrometheusRequestDurations(q => q.MetricName = metricName);
-        _app.Build();
+        UseBuildApp(q => q.MetricName = metricName);
 
         _registry.TryGet(metricName, out var collector);
 
         Assert.NotNull(collector);
+    }
+
+    [Fact]
+    public void Use_WithCustomMetricName_DefaultCollectorIsNull()
+    {
+        UseBuildApp(q => q.MetricName = "custom_name");
+
+        _registry.TryGet(Defaults.MetricName, out var collector);
+
+        Assert.Null(collector);
+    }
+
+    [Fact]
+    public void Collector_IsHistogram()
+    {
+        UseBuildApp();
+
+        _registry.TryGet(Defaults.MetricName, out var collector);
+        Assert.IsAssignableFrom<IMetricFamily<IHistogram>>(collector);
+    }
+
+    [Fact]
+    public void Metric_ContainsStatusCodeLabel()
+    {
+        UseBuildApp(q => q.IncludeStatusCode = true);
+
+        _registry.TryGet(Defaults.MetricName, out var collector);
+        var metric = (IMetricFamily<IHistogram>)collector;
+        Assert.Contains(Defaults.LabelNames.StatusCode, metric.LabelNames);
+    }
+
+    [Fact]
+    public void Metric_DoesNotContainStatusCodeLabel()
+    {
+        UseBuildApp(q => q.IncludeStatusCode = false);
+
+        _registry.TryGet(Defaults.MetricName, out var collector);
+        var metric = (IMetricFamily<IHistogram>)collector;
+        Assert.DoesNotContain(Defaults.LabelNames.StatusCode, metric.LabelNames);
+    }
+
+    [Fact]
+    public void Metric_ContainsMethodLabel()
+    {
+        UseBuildApp(q => q.IncludeMethod = true);
+
+        _registry.TryGet(Defaults.MetricName, out var collector);
+        var metric = (IMetricFamily<IHistogram>)collector;
+        Assert.Contains(Defaults.LabelNames.Method, metric.LabelNames);
+    }
+
+    [Fact]
+    public void Metric_DoesNotContainMethodLabel()
+    {
+        UseBuildApp(q => q.IncludeMethod = false);
+
+        _registry.TryGet(Defaults.MetricName, out var collector);
+        var metric = (IMetricFamily<IHistogram>)collector;
+        Assert.DoesNotContain(Defaults.LabelNames.Method, metric.LabelNames);
+    }
+
+    [Fact]
+    public void Metric_ContainsControllerLabel()
+    {
+        UseBuildApp(q => q.IncludeController = true);
+
+        _registry.TryGet(Defaults.MetricName, out var collector);
+        var metric = (IMetricFamily<IHistogram>)collector;
+        Assert.Contains(Defaults.LabelNames.Controller, metric.LabelNames);
+    }
+
+    [Fact]
+    public void Metric_DoesNotContainControllerLabel()
+    {
+        UseBuildApp(q => q.IncludeController = false);
+
+        _registry.TryGet(Defaults.MetricName, out var collector);
+        var metric = (IMetricFamily<IHistogram>)collector;
+        Assert.DoesNotContain(Defaults.LabelNames.Controller, metric.LabelNames);
+    }
+
+    [Fact]
+    public void Metric_ContainsActionLabel()
+    {
+        UseBuildApp(q => q.IncludeAction = true);
+
+        _registry.TryGet(Defaults.MetricName, out var collector);
+        var metric = (IMetricFamily<IHistogram>)collector;
+        Assert.Contains(Defaults.LabelNames.Action, metric.LabelNames);
+    }
+
+    [Fact]
+    public void Metric_DoesNotContainActionLabel()
+    {
+        UseBuildApp(q => q.IncludeAction = false);
+
+        _registry.TryGet(Defaults.MetricName, out var collector);
+        var metric = (IMetricFamily<IHistogram>)collector;
+        Assert.DoesNotContain(Defaults.LabelNames.Action, metric.LabelNames);
+    }
+
+    [Fact]
+    public void Metric_ContainsPathLabel()
+    {
+        UseBuildApp(q => q.IncludePath = true);
+
+        _registry.TryGet(Defaults.MetricName, out var collector);
+        var metric = (IMetricFamily<IHistogram>)collector;
+        Assert.Contains(Defaults.LabelNames.Path, metric.LabelNames);
+    }
+
+    [Fact]
+    public void Metric_DoesNotContainPathLabel()
+    {
+        UseBuildApp(q => q.IncludePath = false);
+
+        _registry.TryGet(Defaults.MetricName, out var collector);
+        var metric = (IMetricFamily<IHistogram>)collector;
+        Assert.DoesNotContain(Defaults.LabelNames.Path, metric.LabelNames);
+    }
+
+    [Fact]
+    public void Metric_ContainsCustomLabel()
+    {
+        UseBuildApp(q => q.CustomLabels = new Dictionary<string, Func<string>>
+        {
+            {
+                "custom_label", () => "custom_value"
+            }
+        });
+
+        _registry.TryGet(Defaults.MetricName, out var collector);
+        var metric = (IMetricFamily<IHistogram>)collector;
+        Assert.Contains("custom_label", metric.LabelNames);
+        Assert.DoesNotContain("custom_value", metric.LabelNames);
+    }
+
+    [Fact]
+    public void Metric_DoesNotContainCustomLabel()
+    {
+        UseBuildApp();
+
+        _registry.TryGet(Defaults.MetricName, out var collector);
+        var metric = (IMetricFamily<IHistogram>)collector;
+        Assert.DoesNotContain("custom_label", metric.LabelNames);
+    }
+
+    private void UseBuildApp(Action<HttpRequestDurationsOptions> setupOptions = null)
+    {
+        _app.UsePrometheusRequestDurations(setupOptions).Build();
     }
 }
